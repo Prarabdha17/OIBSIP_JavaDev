cbdfd94a71aaae26686f6a2044efe8c351727780
Prarabdha17 <prarabdha.dabhade@gmail.com>
2025-01-06 11:07:35 +0530

    Update on Task2
    Completed the logic of the game and do some changes in the code for the proper output.

--- Oasis_ProjectTasks/src/Project_Tasks/Task2_Number_Guessing_Game.java
+++ Oasis_ProjectTasks/src/Project_Tasks/Task2_Number_Guessing_Game.java
@@ -7,7 +7,7 @@ public class Task2_Number_Guessing_Game
 	
 	public static int MIN_RANGE = 1;
 	public static int MAX_RANGE = 100;
-	public static int MAX_ATTEMPTS = 6;
+	public static int MAX_ATTEMPTS = 10;
 	public static int MAX_ROUNDS = 3;
 	
 	public static void main(String[] args)
@@ -18,18 +18,18 @@ public class Task2_Number_Guessing_Game
 		
 		System.out.println("-----Welcome to Number Guessing Game-----\n");
 		System.out.println("Total Number of Rounds = 3");
-		System.out.println("Number of Attempts For Round = 6\n");
+		System.out.println("Number of Attempts For a Round = 10\n");
 		
-		for(int i = 1;i<MAX_ROUNDS;i++) 
+		for(int i = 1;i<=MAX_ROUNDS;i++) 
 		{
 			int number = random.nextInt(MAX_RANGE) + MIN_RANGE;
 			int current_attempts = 0;
 			
-			System.out.printf("Current Round: %d: Guess the Number between %d and %d in %d attempts.\n",i,MIN_RANGE,MAX_RANGE,MAX_ATTEMPTS);
+			System.out.printf("\nRound Number: %d: Guess the Number between %d to %d in %d attempts.\n",i,MIN_RANGE,MAX_RANGE,MAX_ATTEMPTS);
 			
 			while(current_attempts < MAX_ATTEMPTS) 
 			{
-				System.out.println("Enter Your Guess: ");
+				System.out.println("\nEnter Your Guess: ");
 				int guess_number = sc.nextInt();
 				current_attempts = current_attempts + 1;
 				
@@ -39,11 +39,31 @@ public class Task2_Number_Guessing_Game
 					int score = MAX_ATTEMPTS - current_attempts;
 					totalscore =+ score;
 					System.out.println("Bravooo!!! You Successfully guessed the Number");
-					System.out.printf("Attempts: %d. Round score: %d",current_attempts,score);
+					
+					
+					System.out.printf("Attempts taken: %d. Round score: %d\n",current_attempts,score);
 					break;
 				}
+				else if(guess_number<number)
+				{
+					System.out.printf("The number is greater than %d. Attempts Left = %d\n",guess_number,MAX_ATTEMPTS - current_attempts);
+					
+				}
+				else if(guess_number>number)
+				{
+					System.out.printf("The number is Lesser than %d. Attempts Left = %d\n",guess_number,MAX_ATTEMPTS - current_attempts);				
+				}
+				
 			}
+			if(current_attempts == MAX_ATTEMPTS) 
+			{
+				System.out.printf("You Lost In The Round = %d. Attempts = 0",i);
+				System.out.printf("The Random Number is %d\n",number);
+			}
+			
 		}
+		System.out.printf("Game Over..Total Score = %d\n",totalscore);
+		sc.close();
 	}
 
 }
